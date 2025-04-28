6e2ea5521515f5c310f812412881408737d1cfca
sifou20ws <blvsifo3@gmail.com>
2025-04-28 10:00:00 +0100

    adding new feature to portfolio

---
+++ lib/features/projects/bindings/project_detail_binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+import '../controllers/project_detail_controller.dart';
+
+class ProjectDetailBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => ProjectDetailController(Get.find()));
+  }
+}
