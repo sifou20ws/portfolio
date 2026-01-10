d4a0f27581b47c244c1a31b103f0f749af54b44e
sifou20ws <blvsifo3@gmail.com>
2026-01-10 10:00:00 +0100

    adding new feature to portfolio

--- lib/features/projects/controllers/project_detail_controller.dart
+++ lib/features/projects/controllers/project_detail_controller.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../../app/routes/app_routes.dart';
+import '../../../core/config/app_config.dart';
 import '../../../data/models/project.dart';
 import '../../../data/repositories/project_repository.dart';
 import 'projects_controller.dart';
@@ -44,6 +46,15 @@ class ProjectDetailController extends GetxController {
     } catch (_) {
       status.value = LoadStatus.error;
     }
+  }
+
+  @override
+  void onClose() {
+    // Leaving the project page: restore the site title.
+    SystemChrome.setApplicationSwitcherDescription(
+      const ApplicationSwitcherDescription(label: AppConfig.siteTitle),
+    );
+    super.onClose();
   }
 
   /// Pops back to the list, or replaces the stack with home if this page was
